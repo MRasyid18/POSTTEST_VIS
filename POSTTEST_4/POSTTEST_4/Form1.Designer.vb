75f42df6745442890a7cc54b850c1272c983a6fe
MRasyid18 <mrasyid18102005@gmail.com>
2026-04-07 21:08:40 +0800

    POSTTEST 4

---
+++ POSTTEST_4/POSTTEST_4/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        InputDataToolStripMenuItem = New ToolStripMenuItem()
+        LihatKartuToolStripMenuItem = New ToolStripMenuItem()
+        SimpanDataToolStripMenuItem = New ToolStripMenuItem()
+        BukaDataToolStripMenuItem = New ToolStripMenuItem()
+        KeluarToolStripMenuItem = New ToolStripMenuItem()
+        Label1 = New Label()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        MenuStrip1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {InputDataToolStripMenuItem, LihatKartuToolStripMenuItem, SimpanDataToolStripMenuItem, BukaDataToolStripMenuItem, KeluarToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 28)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' InputDataToolStripMenuItem
+        ' 
+        InputDataToolStripMenuItem.Name = "InputDataToolStripMenuItem"
+        InputDataToolStripMenuItem.Size = New Size(93, 24)
+        InputDataToolStripMenuItem.Text = "Input Data"
+        ' 
+        ' LihatKartuToolStripMenuItem
+        ' 
+        LihatKartuToolStripMenuItem.Name = "LihatKartuToolStripMenuItem"
+        LihatKartuToolStripMenuItem.Size = New Size(94, 24)
+        LihatKartuToolStripMenuItem.Text = "Lihat Kartu"
+        ' 
+        ' SimpanDataToolStripMenuItem
+        ' 
+        SimpanDataToolStripMenuItem.Name = "SimpanDataToolStripMenuItem"
+        SimpanDataToolStripMenuItem.Size = New Size(109, 24)
+        SimpanDataToolStripMenuItem.Text = "Simpan Data"
+        ' 
+        ' BukaDataToolStripMenuItem
+        ' 
+        BukaDataToolStripMenuItem.Name = "BukaDataToolStripMenuItem"
+        BukaDataToolStripMenuItem.Size = New Size(95, 24)
+        BukaDataToolStripMenuItem.Text = " Buka Data"
+        ' 
+        ' KeluarToolStripMenuItem
+        ' 
+        KeluarToolStripMenuItem.Name = "KeluarToolStripMenuItem"
+        KeluarToolStripMenuItem.Size = New Size(65, 24)
+        KeluarToolStripMenuItem.Text = "Keluar"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(311, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 41)
+        Label1.TabIndex = 1
+        Label1.Text = "JUDUL"
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Location = New Point(41, 103)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(566, 319)
+        TabControl1.TabIndex = 2
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Location = New Point(4, 29)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(558, 286)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "TabPage1"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Location = New Point(4, 29)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(242, 92)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "TabPage2"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TabControl1)
+        Controls.Add(Label1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        TabControl1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents InputDataToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LihatKartuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SimpanDataToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BukaDataToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KeluarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+
+End Class
